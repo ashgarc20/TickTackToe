1aa3260e73d7279513a51a70ea127f77d05834aa
Ashley <ashleyagarcia@student.hartnell.edu>
2019-01-30 05:47:21 +0000

    Added a welcome statement to my game.

---
+++ tickTackToe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{ 
+    // Going to start off by asking the player if it is player v. player or player v. computer 
+    int gameMode;
+    
+    cout << " Hello! Welcome to Ash's Tick Tack Toe. " << endl;
+    
+    
+    return 0;
+}
